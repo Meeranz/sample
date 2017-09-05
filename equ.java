68816a9baf11ca63164864c98878bd1d38462bb9
Mohamed Yaseen A <Mohamed Yaseen A@dell>
2017-09-05 06:14:59 +0530

    equ check prgm

---
+++ equ.java
@@ -0,0 +1,21 @@
+class equ
+{
+public static void main(String[] args0)
+{
+Scanner sc = new Scanner(System.in);
+int n = sc.nextInt();
+int[] a= new int[n];
+for(int i=0;i<n;i++)
+{
+a[i]=sc.nextInt();
+}
+for(int i=0;i<n;i++)
+{
+if(a[i]==i)
+{
+System.out.println("the number"+a[i]+"equals its index"+i);
+}
+}
+
+}
+}
